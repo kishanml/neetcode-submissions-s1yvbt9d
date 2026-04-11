ab51bd7f39427bee84f615f2430f4d5f9b62a969
Kishan Mishra <kishanmldev@gmail.com>
2026-04-11 08:04:14 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        
+        m = len(board)
+        n = len(board[0])
+
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r//3,c//3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
+        return True
